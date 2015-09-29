9b5f2974b033da6fb8f65b74108322dcc5eb3f38
Andrew Carlson <rotexdev@gmail.com>
2015-09-28 20:30:54 -0700

    Remove nineoldandroid and update dependencies.

--- library/src/com/sothree/slidinguppanel/SlidingUpPanelLayout.java
+++ library/src/com/sothree/slidinguppanel/SlidingUpPanelLayout.java
@@ -8,7 +8,6 @@ import android.graphics.Paint;
 import android.graphics.PixelFormat;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
-import android.os.Build;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.support.annotation.NonNull;
@@ -24,7 +23,6 @@ import android.view.accessibility.AccessibilityEvent;
 import android.widget.ListView;
 import android.widget.ScrollView;
 
-import com.nineoldandroids.view.animation.AnimatorProxy;
 import com.sothree.slidinguppanel.library.R;
 
 public class SlidingUpPanelLayout extends ViewGroup {
@@ -1148,12 +1146,7 @@ public class SlidingUpPanelLayout extends ViewGroup {
     @SuppressLint("NewApi")
     private void applyParallaxForCurrentSlideOffset() {
         if (mParallaxOffset > 0) {
-            int mainViewOffset = getCurrentParalaxOffset();
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-                mMainView.setTranslationY(mainViewOffset);
-            } else {
-                AnimatorProxy.wrap(mMainView).setTranslationY(mainViewOffset);
-            }
+            mMainView.setTranslationY(getCurrentParalaxOffset());
         }
     }
 
